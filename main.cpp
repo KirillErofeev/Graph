7460c78b9d53ec939ea9e1166f8e698117583b90
Kirill Erofeev <krllerof@gmail.com>
2016-05-15 20:26:23 +0300

    Tests

--- main.cpp
+++ main.cpp
@@ -11,9 +11,11 @@ int main(){
 
     double *weights = new double[2000];
     verFromVerToDistance v;
+
     for (size_t d = 0; d < 2; ++d) {
         std::ofstream out("tests/" + std::to_string(d) + ".txt");
-        for (size_t n = 200 ; n < 2000; n+=200) {
+        std::cout << "=======naiveDijkstraHeapForAll========" << "\n";
+        for (size_t n = 200 ; n <= 2000; n+=200) {
             Graph g(adjacencyMatrix(n, d));
             auto start = std::chrono::high_resolution_clock::now();
             naiveDijkstraHeapForAll(g, v);
@@ -21,13 +23,38 @@ int main(){
             std::chrono::duration<double> diff = end-start;
             std::cout << n << "\t" << diff.count() << "\n";
         }
-    }
 
-    auto start = std::chrono::high_resolution_clock::now();
-    Graph g( adjacencyMatrix(2000, 15) );
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end-start;
-    std::cout << diff.count() << std::endl;
+        std::cout << "=======floyd========" << "\n";
+        for (size_t n = 200 ; n <= 2000; n+=200) {
+            Graph g(adjacencyMatrix(n, d));
+            auto start = std::chrono::high_resolution_clock::now();
+            floyd(g, v);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> diff = end-start;
+            std::cout << n << "\t" << diff.count() << "\n";
+        }
+
+        std::cout << "=======naiveDijkstraForAll========" << "\n";
+        for (size_t n = 200 ; n <= 2000; n+=200) {
+            Graph g(adjacencyMatrix(n, d));
+            auto start = std::chrono::high_resolution_clock::now();
+            naiveDijkstraForAll(g, v);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> diff = end-start;
+            std::cout << n << "\t" << diff.count() << "\n";
+        }
+
+        std::cout << "=======bellmanForAll========" << "\n";
+        for (size_t n = 200 ; n <= 2000; n+=200) {
+            Graph g(adjacencyMatrix(n, d));
+            auto start = std::chrono::high_resolution_clock::now();
+            bellmanForAll(g, v);
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> diff = end-start;
+            std::cout << n << "\t" << diff.count() << "\n";
+        }
+
+    }
 
 
 
